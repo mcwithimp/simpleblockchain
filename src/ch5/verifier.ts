6eafc1f644658e473f9842e2c4587000e66320e4
mcwithimp <mcwithimp@tezoskorea.com>
2019-12-19 15:58:56 +0900

    block and blockchain verifier

--- src/ch5/verifier.ts
+++ src/ch5/verifier.ts
@@ -3,6 +3,7 @@ import { getHead, getBlockchain } from './blockchain'
 import { Blockchain, Block } from './types/block.js'
 import isEqual from 'lodash.isequal'
 import { sha256 } from '../lib/crypto.js'
+import { difficultyConstant } from './miner.js'
 
 export const getHash = (data: object): string => sha256(JSON.stringify(data))
 export const getTimestamp = () => Math.floor(new Date().getTime() / 1000)
@@ -12,22 +13,13 @@ export const getTimestamp = () => Math.floor(new Date().getTime() / 1000)
 // A timestamp is accepted as valid 
 // if it is greater than the median timestamp of previous 11 blocks, 
 // and less than the network - adjusted time + 2 hours. (2 hours = 12 times BLOCK_INTERVAL)
-// Adjusting time is because of asynchronous time between processes assumption 
-// which we don't care 
-export const verifyTimestamp = (level: number, timestamp: number) : boolean => {
-  const head = getHead()
-  if (level > head.header.level + 1) return false
-
-  const lb = Math.max(level - 11, 0)
-  const left = getBlockchain()[lb].header.timestamp
-  const right = getBlockchain()[level - 1].header.timestamp
-  // rounded down for generosity
-  const lowerBound = Math.floor((left + right) / 2)
+// Adjusting time is because of asynchronous time between processes assumption which we don't care 
+export const verifyTimestamp = (previousTime: number, newTime: number) : boolean => {
+  const lowerBound = previousTime - (BLOCK_INTERVAL * 6)
   const upperBound = getTimestamp() + (BLOCK_INTERVAL * 12)
-
   return (
-    lowerBound <= timestamp &&
-    timestamp <= upperBound
+    lowerBound <= newTime &&
+    newTime <= upperBound
   )
 }
 
@@ -35,14 +27,24 @@ export const verifyBlock = (block: Block) : boolean => {
 
   const { hash, header, transactions } = block
 
-  sha256()
+  // check hash values are calculated correctly
+  if (getHash(transactions) !== header.txsHash) {
+    console.log("TxsHash is not valid!")
+    return false
+  } else if(getHash(header) !== hash) {
+    console.log("BlockHash is not valid!")
+    return false
+  }
 
-  
-
+  // verify proof of work
+  const difficulty = header.difficulty
+  const target = BigInt(difficultyConstant / difficulty)
+  if (BigInt(`0x${hash}`) >= target) {
+    console.log("PoW is not valid!")
+    return false
+  }
 
   return true
-
-
 }
 
 export const verifyChain = (candidateChain: Blockchain) : boolean => {
@@ -57,18 +59,34 @@ export const verifyChain = (candidateChain: Blockchain) : boolean => {
       console.log('The remote genesis block is invalid!')
       return false
     }
-  } else if (
+  } else if ( // if there are missing links
     localChain[lb.header.level - 1] === undefined ||
     localChain[lb.header.level - 1].hash !== lb.header.previousHash) {
-    console.log("The candidate chain can not be connected to localChain")
+    console.log("The candidate chain can not be connected to localChain!")
     return false
   }
 
   // now we can replace the part or all of localChain with candidateChain
+  // copy and merge the chain
+  const merged = localChain.slice(0, lb.header.level).concat(candidateChain)
 
-  
-    
+  // check level, timestamp, previousHash and block itself 
+  const isChainValid = merged.slice(1).every((block, _idx) => {
+    if (block.header.level !== merged[_idx].header.level + 1) {
+      console.log("Block level is not valid!")
+      return false
+    } else if (block.header.previousHash !== merged[_idx].hash) {
+      console.log("Previous hash is not valid!")
+      return false
+    } else if (verifyTimestamp(merged[_idx].header.timestamp, block.header.timestamp) === false) {
+      console.log("Timestamp value is not valid!")
+      return false
+    } else if (verifyBlock(block) === false) return false
 
+    return true
+  })
+
+  return isChainValid
 }
 
 
